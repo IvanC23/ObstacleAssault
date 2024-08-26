0878a239a10ca9643c5fc1df250ec98222acb0cf
Ivan <92627438+IvanC23@users.noreply.github.com>
2024-08-26 10:25:40 +0200

    First commit after course

---
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	UE_LOG(LogTemp, Display, TEXT("My move distance is %f"), MoveDistance);
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MovePlatform(DeltaTime);
+
+	RotatePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+
+	CurrentLocation += PlatformVelocity * DeltaTime;
+
+	SetActorLocation(CurrentLocation);
+
+	float DistanceMoved = FVector::Distance(StartLocation, CurrentLocation);
+
+	if (DistanceMoved >= MoveDistance)
+	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(StartLocation);
+		PlatformVelocity *= -1;
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
